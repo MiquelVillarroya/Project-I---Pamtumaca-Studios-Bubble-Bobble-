a918a9e68d6197d22ec88525ab1b6cfe239b9597
noapont <17pontnoa@gmail.com>
2024-04-18 20:33:52 +0200

    Create level 2 and fix the menu image

--- BubbleBobble/src/TileMap.cpp
+++ BubbleBobble/src/TileMap.cpp
@@ -35,8 +35,16 @@ void TileMap::InitTileDictionary()
 
 	dict_rect[(int)Tile::BLOCK_LVL1] = { 0,  0, n + sx, n + sy};
 	dict_rect[(int)Tile::PLAT_LVL1] = { 0,  0, n + sx, n + sy};
-
 	dict_rect[(int)Tile::NUMBER_BLOCK_LVL1_1] = { 2*n,  0, n + sx, n + sy };
+	dict_rect[(int)Tile::NUMBER_BLOCK_LVL1_2] = { 4 * n,  0, n + sx, n + sy };
+
+	dict_rect[(int)Tile::BLOCK_LVL2] = { 0,  2*n, n + sx, n + sy };
+	dict_rect[(int)Tile::PLAT_LVL2] = { 0,  2*n, n + sx, n + sy };
+	dict_rect[(int)Tile::WALL_LVL2_1] = { 2*n,  2*n, n + sx, n + sy };
+	dict_rect[(int)Tile::WALL_LVL2_2] = {3*n,  2*n, n + sx, n + sy};
+	dict_rect[(int)Tile::WALL_LVL2_3] = { 5 * n,  2 * n, n + sx, n + sy };
+	dict_rect[(int)Tile::WALL_LVL2_4] = { 6 * n,  2 * n, n + sx, n + sy };
+	dict_rect[(int)Tile::NUMBER_BLOCK_LVL1_1] = { 2 * n,  0, n + sx, n + sy };
 	dict_rect[(int)Tile::NUMBER_BLOCK_LVL1_2] = { 4 * n,  0, n + sx, n + sy };
 
 	dict_rect[(int)Tile::FOOD_MUSHROOM] = { 0,  0, n + sx, n + sy};
